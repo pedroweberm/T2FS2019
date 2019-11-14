e396c6665ae4747ad438769a8adc43579a01eef2
Gustavo Lorentz <gus.aca.lo@gmail.com>
2019-11-14 01:44:18 -0200

    read buffer

--- src/t2fs.c
+++ src/t2fs.c
@@ -25,198 +25,204 @@ superbloco Super;
 //}
 
 int sal(){
-    char buffer[SECTOR_SIZE];
+    char buffer[SECTOR_SIZE] = {'c'};
     printf(" oiioi\n");
     printf(" %d" ,read_sector(0, buffer));
 
     printf(" li o buffalo %d\n", strlen(buffer));
     int i = 0;
-    for (i = 0; i < strlen(buffer); i ++)
-        printf("%c\n", buffer[i]);
+    int j = 0;
+
+    for (i = 0; i < 256; i ++) {
+        for (j = 0; j < 8; j ++){
+            printf("%d", !!((buffer[i] << j) & 0x80));
+        }
+        printf("\n");
+    }
     return 0;
 }
-int readSuper()
-{
-    unsigned char buffer[SECTOR_SIZE];
-    if(read_sector(0, buffer) != 0)
-    {
-        printf("Error: Failed reading sector 0!\n");
-        return -1;
-    }
-    strncpy(Super.id, (char*)buffer, 4);
-    Super.version = *( (DWORD*)(buffer + 4) );
-    Super.superblockSize = *( (WORD*)(buffer + 6) );
-    Super.freeBlocksBitmapSize = *( (WORD*)(buffer + 8) );
-    Super.freeInodeBitmapSize = *( (WORD*)(buffer + 10) );
-    Super.inodeAreaSize = *( (WORD*)(buffer + 12) );
-    Super.blockSize = *( (WORD*)(buffer + 14) );
-    Super.diskSize = *( (DWORD*)(buffer + 16) );
-    return 0;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Informa a identificação dos desenvolvedores do T2FS.
------------------------------------------------------------------------------*/
-int identify2 (char *name, int size)
-{
-    int i = 0;
-    char names[71] = {0};
-
-    strcpy (names, "Demetrio Boeira - 297693\nGustavo Lorentz - 287681\nPedro Weber - 287678\n");
-
-    for (i=0; i < size; i++)
-    {
-        name[i] = names[i];
-    }
-
-    if (strcmp(names, name) == 0)
-    {
-        return 0;
-    }
-
-
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Formata logicamente uma partição do disco virtual t2fs_disk.dat para o sistema de
-		arquivos T2FS definido usando blocos de dados de tamanho
-		corresponde a um múltiplo de setores dados por sectors_per_block.
------------------------------------------------------------------------------*/
-int format2(int partition, int sectors_per_block)
-{
-
-    for(i = 0; i < Super.diskSize; i++)
-        setBitmap2(BITMAP_DADOS, i, 0);
-
-    for ( i = 0; i < Super.SuperSize; i++)
-        setBitmap2(BITMAP_DADOS, i, 1);
-
-    for ( i = 0; i < Super.freeBlockBitmapSize; i++)
-        setBitmap2(BITMAP_DADOS, Super.SuperSize + i, 1);
-
-    for ( i =0; i < Super.freeInodeBitmapSize; i++)
-        setBitmap2(BITMAP_DADOS, Super.SuperSize + Super.freeBlocksBitmapSize + i, 1);
-
-    for ( i = 0; i< Super.inodeAreaSize; i++)
-        setBitmap2(BITMAP_DADOS, Super.SuperSize + Super.freeBlocksBitmapSize + Super.freeInodeBitmapSize + i, 1);
-
-    for (i = 0; i < Super.inodeAreaSize * iNodesPerSector * Super.blockSize; i++)
-        setBitmap2(BITMAP_INODE, i, 0);
-
-    setBitmap2(BITMAP_INODE, 0, 1);
-
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Monta a partição indicada por "partition" no diretório raiz
------------------------------------------------------------------------------*/
-int mount(int partition)
-{
-
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Desmonta a partição atualmente montada, liberando o ponto de montagem.
------------------------------------------------------------------------------*/
-int unmount(void)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função usada para criar um novo arquivo no disco e abrí-lo,
-		sendo, nesse último aspecto, equivalente a função open2.
-		No entanto, diferentemente da open2, se filename referenciar um
-		arquivo já existente, o mesmo terá seu conteúdo removido e
-		assumirá um tamanho de zero bytes.
------------------------------------------------------------------------------*/
-FILE2 create2 (char *filename)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função usada para remover (apagar) um arquivo do disco.
------------------------------------------------------------------------------*/
-int delete2 (char *filename)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função que abre um arquivo existente no disco.
------------------------------------------------------------------------------*/
-FILE2 open2 (char *filename)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função usada para fechar um arquivo.
------------------------------------------------------------------------------*/
-int close2 (FILE2 handle)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função usada para realizar a leitura de uma certa quantidade
-		de bytes (size) de um arquivo.
------------------------------------------------------------------------------*/
-int read2 (FILE2 handle, char *buffer, int size)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função usada para realizar a escrita de uma certa quantidade
-		de bytes (size) de  um arquivo.
------------------------------------------------------------------------------*/
-int write2 (FILE2 handle, char *buffer, int size)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função que abre um diretório existente no disco.
------------------------------------------------------------------------------*/
-DIR2 opendir2 (void)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função usada para ler as entradas de um diretório.
------------------------------------------------------------------------------*/
-int readdir2 (DIRENT2 *dentry)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função usada para fechar um diretório.
------------------------------------------------------------------------------*/
-int closedir2 (void)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função usada para criar um caminho alternativo (softlink)
------------------------------------------------------------------------------*/
-int sln2 (char *linkname, char *filename)
-{
-    return -1;
-}
-
-/*-----------------------------------------------------------------------------
-Função:	Função usada para criar um caminho alternativo (hardlink)
------------------------------------------------------------------------------*/
-int hln2(char *linkname, char *filename)
-{
-    return -1;
-}
-
-
-
+//int readSuper()
+//{
+//    unsigned char buffer[SECTOR_SIZE];
+//    if(read_sector(0, buffer) != 0)
+//    {
+//        printf("Error: Failed reading sector 0!\n");
+//        return -1;
+//    }
+//    strncpy(Super.id, (char*)buffer, 4);
+//    Super.version = *( (DWORD*)(buffer + 4) );
+//    Super.superblockSize = *( (WORD*)(buffer + 6) );
+//    Super.freeBlocksBitmapSize = *( (WORD*)(buffer + 8) );
+//    Super.freeInodeBitmapSize = *( (WORD*)(buffer + 10) );
+//    Super.inodeAreaSize = *( (WORD*)(buffer + 12) );
+//    Super.blockSize = *( (WORD*)(buffer + 14) );
+//    Super.diskSize = *( (DWORD*)(buffer + 16) );
+//    return 0;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Informa a identificação dos desenvolvedores do T2FS.
+//-----------------------------------------------------------------------------*/
+//int identify2 (char *name, int size)
+//{
+//    int i = 0;
+//    char names[71] = {0};
+//
+//    strcpy (names, "Demetrio Boeira - 297693\nGustavo Lorentz - 287681\nPedro Weber - 287678\n");
+//
+//    for (i=0; i < size; i++)
+//    {
+//        name[i] = names[i];
+//    }
+//
+//    if (strcmp(names, name) == 0)
+//    {
+//        return 0;
+//    }
+//
+//
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Formata logicamente uma partição do disco virtual t2fs_disk.dat para o sistema de
+//		arquivos T2FS definido usando blocos de dados de tamanho
+//		corresponde a um múltiplo de setores dados por sectors_per_block.
+//-----------------------------------------------------------------------------*/
+//int format2(int partition, int sectors_per_block)
+//{
+//
+//    for(i = 0; i < Super.diskSize; i++)
+//        setBitmap2(BITMAP_DADOS, i, 0);
+//
+//    for ( i = 0; i < Super.SuperSize; i++)
+//        setBitmap2(BITMAP_DADOS, i, 1);
+//
+//    for ( i = 0; i < Super.freeBlockBitmapSize; i++)
+//        setBitmap2(BITMAP_DADOS, Super.SuperSize + i, 1);
+//
+//    for ( i =0; i < Super.freeInodeBitmapSize; i++)
+//        setBitmap2(BITMAP_DADOS, Super.SuperSize + Super.freeBlocksBitmapSize + i, 1);
+//
+//    for ( i = 0; i< Super.inodeAreaSize; i++)
+//        setBitmap2(BITMAP_DADOS, Super.SuperSize + Super.freeBlocksBitmapSize + Super.freeInodeBitmapSize + i, 1);
+//
+//    for (i = 0; i < Super.inodeAreaSize * iNodesPerSector * Super.blockSize; i++)
+//        setBitmap2(BITMAP_INODE, i, 0);
+//
+//    setBitmap2(BITMAP_INODE, 0, 1);
+//
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Monta a partição indicada por "partition" no diretório raiz
+//-----------------------------------------------------------------------------*/
+//int mount(int partition)
+//{
+//
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Desmonta a partição atualmente montada, liberando o ponto de montagem.
+//-----------------------------------------------------------------------------*/
+//int unmount(void)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função usada para criar um novo arquivo no disco e abrí-lo,
+//		sendo, nesse último aspecto, equivalente a função open2.
+//		No entanto, diferentemente da open2, se filename referenciar um
+//		arquivo já existente, o mesmo terá seu conteúdo removido e
+//		assumirá um tamanho de zero bytes.
+//-----------------------------------------------------------------------------*/
+//FILE2 create2 (char *filename)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função usada para remover (apagar) um arquivo do disco.
+//-----------------------------------------------------------------------------*/
+//int delete2 (char *filename)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função que abre um arquivo existente no disco.
+//-----------------------------------------------------------------------------*/
+//FILE2 open2 (char *filename)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função usada para fechar um arquivo.
+//-----------------------------------------------------------------------------*/
+//int close2 (FILE2 handle)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função usada para realizar a leitura de uma certa quantidade
+//		de bytes (size) de um arquivo.
+//-----------------------------------------------------------------------------*/
+//int read2 (FILE2 handle, char *buffer, int size)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função usada para realizar a escrita de uma certa quantidade
+//		de bytes (size) de  um arquivo.
+//-----------------------------------------------------------------------------*/
+//int write2 (FILE2 handle, char *buffer, int size)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função que abre um diretório existente no disco.
+//-----------------------------------------------------------------------------*/
+//DIR2 opendir2 (void)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função usada para ler as entradas de um diretório.
+//-----------------------------------------------------------------------------*/
+//int readdir2 (DIRENT2 *dentry)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função usada para fechar um diretório.
+//-----------------------------------------------------------------------------*/
+//int closedir2 (void)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função usada para criar um caminho alternativo (softlink)
+//-----------------------------------------------------------------------------*/
+//int sln2 (char *linkname, char *filename)
+//{
+//    return -1;
+//}
+//
+///*-----------------------------------------------------------------------------
+//Função:	Função usada para criar um caminho alternativo (hardlink)
+//-----------------------------------------------------------------------------*/
+//int hln2(char *linkname, char *filename)
+//{
+//    return -1;
+//}
+//
+//
+//
