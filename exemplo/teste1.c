ea6057f9cc5d0c2ace5f121bb08f0d2c2bceb485
Gustavo <gus.aca.lo@gmail.com>
2019-11-19 16:20:41 -0200

    Changed structure name and fixed occurrences

--- exemplo/teste1.c
+++ exemplo/teste1.c
@@ -5,18 +5,12 @@
 int main()
 {
 
-    DIRENT2 dentry;
-    strcpy(dentry.name, "Salzinho\0");
-    dentry.TypeVal = 1;
-    dentry.inodeNumber = 5;
-
     printf("Main\n");
     initT2FS();
 //    logMbr();
     format2(0, 2);
     mount(0);
     opendir2();
-    readdir2(&dentry);
 //    readSuper(0);
     return 0;
 }
